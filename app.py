7a40a0bdca00edc84894c522cbfbefa95a2f67cf
Sandeep Kashyap <sandeepramaswamykashyap@gmail.com>
2026-01-15 14:34:21 +0530

    Update App Title to v1.0.0

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="HireLink v1.2 (Patched)", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="HireLink v1.0.0", layout="wide", initial_sidebar_state="expanded")
 
 # --- RESUME SEEDING DATA (Sanitized) ---
 SEED_RESUME_DATA = {
