3210b994836a8e3d40529a95d3f9903e7873c27e
Sandeep Kashyap <sandeepramaswamykashyap@gmail.com>
2026-01-18 21:28:57 +0530

    Fix: Restore Import Correctly (v2.46)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import streamlit as st
 st.set_page_config(
-        page_title="HireLink v2.45 (Stable)",
+        page_title="HireLink v2.46 (Stable)",
         page_icon="💸",
         layout="wide",
         initial_sidebar_state="expanded"
