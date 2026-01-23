f5fd6d6e4ae8e2fe456d155ed9eed40f8af39807
Sandeep Kashyap <sandeepramaswamykashyap@gmail.com>
2026-01-23 09:43:18 +0530

    Build: Bump version to v2.5 to trigger new deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(
-        page_title="HireLink v2.46 (Stable)",
-        page_icon="💸",
+        page_title="HireLink v2.5 (Core Upgrade)",
+        page_icon="🤖",
         layout="wide",
         initial_sidebar_state="expanded"
     )
